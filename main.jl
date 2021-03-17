d3f75bb67845aa17d1cd87d4deda50f2785785ab
antonconcheiro <anton.concheiro@udc.es>
2021-03-17 20:17:12 +0100

    Añadido validacion y test

--- main.jl
+++ main.jl
@@ -1,7 +1,8 @@
 using FileIO
 using Images
 using Statistics
-using Flux
+using Flux.Losses
+using Random
 
 # Functions that allow the conversion from images to Float64 arrays
 imageToGrayArray(image:: Array{RGB{Normed{UInt8,8}},2}) = convert(Array{Float64,2}, gray.(Gray.(image)));
@@ -199,55 +200,119 @@ function buildClassANN(numInputs::Int64, topology::Array{Int64,1}, numOutputs::I
     return ann;
 end;
 
-function trainClassANN(topology::Array{Int64,1}, inputs::Array{Float64,2}, targets::Array{Bool,2}; maxEpochs::Int64=1000, minLoss::Float64=0.0, learningRate::Float64=0.1)
+function trainClassANN(topology::Array{Int64,1},
+    trainingInputs::Array{Float64,2},   trainingTargets::Array{Bool,2},
+    validationInputs::Array{Float64,2}, validationTargets::Array{Bool,2},
+    testInputs::Array{Float64,2},       testTargets::Array{Bool,2};
+    maxEpochs::Int64=1000, minLoss::Float64=0.0, learningRate::Float64=0.1, maxEpochsVal::Int64=6, showText::Bool=false)
+
     # Se supone que tenemos cada patron en cada fila
-    # Comprobamos que el numero de filas (numero de patrones) coincide
-    @assert(size(inputs,1)==size(targets,1));
+    # Comprobamos que el numero de filas (numero de patrones) coincide tanto en entrenamiento como en validation como test
+    @assert(size(trainingInputs,1)==size(trainingTargets,1));
+    @assert(size(validationInputs,1)==size(validationTargets,1));
+    @assert(size(testInputs,1)==size(testTargets,1));
+    # Comprobamos que el numero de columnas coincide en los grupos de entrenamiento, validacion y test
+    @assert(size(trainingInputs,2)==size(validationInputs,2)==size(testInputs,2));
+    @assert(size(trainingTargets,2)==size(validationTargets,2)==size(testTargets,2));
     # Creamos la RNA
-    ann = buildClassANN(size(inputs,2), topology, size(targets,2));
+    ann = buildClassANN(size(trainingInputs,2), topology, size(trainingTargets,2));
     # Definimos la funcion de loss
     loss(x,y) = (size(y,1) == 1) ? Losses.binarycrossentropy(ann(x),y) : Losses.crossentropy(ann(x),y);
     # Creamos los vectores con los valores de loss y de precision en cada ciclo
     trainingLosses = Float64[];
     trainingAccuracies = Float64[];
+    validationLosses = Float64[];
+    validationAccuracies = Float64[];
+    testLosses = Float64[];
+    testAccuracies = Float64[];
+
     # Empezamos en el ciclo 0
     numEpoch = 0;
 
     # Una funcion util para calcular los resultados y mostrarlos por pantalla
     function calculateMetrics()
-        # Calculamos el loss. Para ello hay que pasar las matrices traspuestas (cada patron en una columna)
-        trainingLoss = loss(inputs', targets');
-        # Calculamos la salida de la RNA. Para ello hay que pasar la matriz de entradas traspuesta (cada patron en una columna). La matriz de salidas tiene un patron en cada columna
-        outputs = ann(inputs');
+        # Calculamos el loss en entrenamiento y test. Para ello hay que pasar las matrices traspuestas (cada patron en una columna)
+        trainingLoss   = loss(trainingInputs',   trainingTargets');
+        validationLoss = loss(validationInputs', validationTargets');
+        testLoss       = loss(testInputs',       testTargets');
+        # Calculamos la salida de la RNA en entrenamiento y test. Para ello hay que pasar la matriz de entradas traspuesta (cada patron en una columna). La matriz de salidas tiene un patron en cada columna
+        trainingOutputs   = ann(trainingInputs');
+        validationOutputs = ann(validationInputs');
+        testOutputs       = ann(testInputs');
         # Para calcular la precision, ponemos 2 opciones aqui equivalentes:
         #  Pasar las matrices con los datos en las columnas. La matriz de salidas ya tiene un patron en cada columna
-        acc = accuracy(outputs, Array{Bool,2}(targets'); dataInRows=false);
+        trainingAcc   = accuracy(trainingOutputs,   Array{Bool,2}(trainingTargets');   dataInRows=false);
+        validationAcc = accuracy(validationOutputs, Array{Bool,2}(validationTargets'); dataInRows=false);
+        testAcc       = accuracy(testOutputs,       Array{Bool,2}(testTargets');       dataInRows=false);
         #  Pasar las matrices con los datos en las filas. Hay que trasponer la matriz de salidas de la RNA, puesto que cada dato esta en una fila
-        acc = accuracy(Array{Float64,2}(outputs'), targets; dataInRows=true);
-        # Mostramos por pantalla el resultado de este ciclo de entrenamiento
-        println("Epoch ", numEpoch, ": loss: ", trainingLoss, ", accuracy: ", 100*acc, " %");
-        return (trainingLoss, acc)
+        trainingAcc   = accuracy(Array{Float64,2}(trainingOutputs'),   trainingTargets;   dataInRows=true);
+        validationAcc = accuracy(Array{Float64,2}(validationOutputs'), validationTargets; dataInRows=true);
+        testAcc       = accuracy(Array{Float64,2}(testOutputs'),       testTargets;       dataInRows=true);
+        # Mostramos por pantalla el resultado de este ciclo de entrenamiento si nos lo han indicado
+        if showText
+            println("Epoch ", numEpoch, ": Training loss: ", trainingLoss, ", accuracy: ", 100*trainingAcc, " % - Validation loss: ", validationLoss, ", accuracy: ", 100*validationAcc, " % - Test loss: ", testLoss, ", accuracy: ", 100*testAcc, " %");
+        end;
+        return (trainingLoss, trainingAcc, validationLoss, validationAcc, testLoss, testAcc)
     end;
 
     # Calculamos las metricas para el ciclo 0 (sin entrenar nada)
-    (trainingLoss, trainingAccuracy) = calculateMetrics();
-    #  y almacenamos el valor de loss y precision en este ciclo
-    push!(trainingLosses, trainingLoss);
-    push!(trainingAccuracies, trainingAccuracy);
+    (trainingLoss, trainingAccuracy, validationLoss, validationAccuracy, testLoss, testAccuracy) = calculateMetrics();
+    #  y almacenamos los valores de loss y precision en este ciclo
+    push!(trainingLosses,       trainingLoss);
+    push!(trainingAccuracies,   trainingAccuracy);
+    push!(validationLosses,     validationLoss);
+    push!(validationAccuracies, validationAccuracy);
+    push!(testLosses,           testLoss);
+    push!(testAccuracies,       testAccuracy);
+
+    # Numero de ciclos sin mejorar el error de validacion y el mejor error de validation encontrado hasta el momento
+    numEpochsValidation = 0; bestValidationLoss = validationLoss;
+    # Cual es la mejor ann que se ha conseguido
+    bestANN = deepcopy(ann);
 
     # Entrenamos hasta que se cumpla una condicion de parada
-    while (numEpoch<maxEpochs) && (trainingLoss>minLoss)
+    while (numEpoch<maxEpochs) && (trainingLoss>minLoss) && (numEpochsValidation<maxEpochsVal)
         # Entrenamos 1 ciclo. Para ello hay que pasar las matrices traspuestas (cada patron en una columna)
-        Flux.train!(loss, params(ann), [(inputs', targets')], ADAM(learningRate));
+        Flux.train!(loss, params(ann), [(trainingInputs', trainingTargets')], ADAM(learningRate));
         # Aumentamos el numero de ciclo en 1
         numEpoch += 1;
         # Calculamos las metricas en este ciclo
-        (trainingLoss, trainingAccuracy) = calculateMetrics()
-        #  y almacenamos el valor de loss y precision en este ciclo
-        push!(trainingLosses, trainingLoss);
-        push!(trainingAccuracies, trainingAccuracy);
+        (trainingLoss, trainingAccuracy, validationLoss, validationAccuracy, testLoss, testAccuracy) = calculateMetrics();
+        #  y almacenamos los valores de loss y precision en este ciclo
+        push!(trainingLosses,       trainingLoss);
+        push!(trainingAccuracies,   trainingAccuracy);
+        push!(validationLosses,     validationLoss);
+        push!(validationAccuracies, validationAccuracy);
+        push!(testLosses,           testLoss);
+        push!(testAccuracies,       testAccuracy);
+        # Aplicamos la parada temprana
+        if (validationLoss<bestValidationLoss)
+            bestValidationLoss = validationLoss;
+            numEpochsValidation = 0;
+            bestANN = deepcopy(ann);
+        else
+            numEpochsValidation += 1;
+        end;
     end;
-    return (ann, trainingLosses, trainingAccuracies);
+    return (bestANN, trainingLosses, validationLosses, testLosses, trainingAccuracies, validationAccuracies, testAccuracies);
+end;
+
+function holdOut(N::Int, P::Float64)
+    @assert ((P>=0.) & (P<=1.));
+    indices = randperm(N);
+    numTrainingInstances = Int(round(N*(1-P)));
+    return (indices[1:numTrainingInstances], indices[numTrainingInstances+1:end]);
+end;
+
+function holdOut(N::Int, Pval::Float64, Ptest::Float64)
+    @assert ((Pval>=0.) & (Pval<=1.));
+    @assert ((Ptest>=0.) & (Ptest<=1.));
+    @assert ((Pval+Ptest)<=1.);
+    # Primero separamos en entrenamiento+validation y test
+    (trainingValidationIndices, testIndices) = holdOut(N, Ptest);
+    # Después separamos el conjunto de entrenamiento+validation
+    (trainingIndices, validationIndices) = holdOut(length(trainingValidationIndices), Pval*N/length(trainingValidationIndices))
+    return (trainingValidationIndices[trainingIndices], trainingValidationIndices[validationIndices], testIndices);
 end;
 
 dataset=loadTrainingDataset();
@@ -265,9 +330,25 @@ println("Tamaño de la matriz de salidas deseadas despues de codificar: ", size(
 topology = [4, 3];
 learningRate = 0.01;
 numMaxEpochs = 1000;
+validationRatio = 0.2;
+testRatio = 0.2;
+maxEpochsVal = 6;
 
 #newInputs = normalizeMinMax(inputs);
 normalizeMinMax!(inputs);
-@assert(all(minimum(newInputs, dims=1) .== 0));
-@assert(all(maximum(newInputs, dims=1) .== 1));
-(ann, trainingLosses, trainingAccuracies) = trainClassANN(topology, inputs, targets; maxEpochs=numMaxEpochs, learningRate=learningRate);
+@assert(all(minimum(inputs, dims=1) .== 0));
+@assert(all(maximum(inputs, dims=1) .== 1));
+
+(trainingIndices, validationIndices, testIndices) = holdOut(size(inputs,1), validationRatio, testRatio);
+trainingInputs    = inputs[trainingIndices,:];
+validationInputs  = inputs[validationIndices,:];
+testInputs        = inputs[testIndices,:];
+trainingTargets   = targets[trainingIndices,:];
+validationTargets = targets[validationIndices,:];
+testTargets       = targets[testIndices,:];
+
+(ann, trainingLosses, trainingAccuracies) = trainClassANN(topology,
+    trainingInputs,   trainingTargets,
+    validationInputs, validationTargets,
+    testInputs,       testTargets;
+    maxEpochs=numMaxEpochs, learningRate=learningRate, maxEpochsVal=maxEpochsVal, showText=true);
