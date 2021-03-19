f8cff8a59e1597d533b8ec3bb6e135d5031464c0
antonconcheiro <anton.concheiro@udc.es>
2021-03-19 02:57:12 +0100

    Añadido matriz confusion, añadido topologia variable

--- main.jl
+++ main.jl
@@ -318,6 +318,238 @@ function holdOut(N::Int, Pval::Float64, Ptest::Float64)
     return (trainingValidationIndices[trainingIndices], trainingValidationIndices[validationIndices], testIndices);
 end;
 
+function classifyOutputs(outputs::Array{Float64,2}; dataInRows::Bool=true)
+    # Miramos donde esta el valor mayor de cada instancia con la funcion findmax
+    (_,indicesMaxEachInstance) = findmax(outputs, dims= dataInRows ? 2 : 1);
+    # Creamos la matriz de valores booleanos con valores inicialmente a false y asignamos esos indices a true
+    outputsBoolean = Array{Bool,2}(falses(size(outputs)));
+    outputsBoolean[indicesMaxEachInstance] .= true;
+    # Comprobamos que efectivamente cada patron solo este clasificado en una clase
+    @assert(all(sum(outputsBoolean, dims=dataInRows ? 2 : 1).==1));
+    return outputsBoolean;
+end;
+
+function confusionMatrix(outputs::Array{Bool,1}, targets::Array{Bool,1})
+    @assert(length(outputs)==length(targets));
+    @assert(length(outputs)==length(targets));
+    # Para calcular la precision y la tasa de error, se puede llamar a las funciones definidas en la practica 2
+    acc         = accuracy(outputs, targets); # Precision, definida previamente en una practica anterior
+    errorRate   = 1 - acc;
+    recall      = mean(  outputs[  targets]); # Sensibilidad
+    specificity = mean(.!outputs[.!targets]); # Especificidad
+    precision   = mean(  targets[  outputs]); # Valor predictivo positivo
+    NPV         = mean(.!targets[.!outputs]); # Valor predictivo negativo
+    # Controlamos que algunos casos pueden ser NaN, y otros no
+    @assert(!isnan(recall) && !isnan(specificity));
+    precision   = isnan(precision) ? 0 : precision;
+    NPV         = isnan(NPV) ? 0 : NPV;
+    # Calculamos F1
+    F1          = (recall==precision==0.) ? 0. : 2*(recall*precision)/(recall+precision);
+    # Reservamos memoria para la matriz de confusion
+    confMatrix = Array{Int64,2}(undef, 2, 2);
+    # Ponemos en las filas los que pertenecen a cada clase (targets) y en las columnas los clasificados (outputs)
+    #  Primera fila/columna: negativos
+    #  Segunda fila/columna: positivos
+    # Primera fila: patrones de clase negativo, clasificados como negativos o positivos
+    confMatrix[1,1] = sum(.!targets .& .!outputs); # VN
+    confMatrix[1,2] = sum(.!targets .&   outputs); # FP
+    # Segunda fila: patrones de clase positiva, clasificados como negativos o positivos
+    confMatrix[2,1] = sum(  targets .& .!outputs); # FN
+    confMatrix[2,2] = sum(  targets .&   outputs); # VP
+    return (acc, errorRate, recall, specificity, precision, NPV, F1, confMatrix)
+end;
+
+confusionMatrix(outputs::Array{Float64,1}, targets::Array{Bool,1}; threshold::Float64=0.5) = confusionMatrix(Array{Bool,1}(outputs.>=threshold), targets);
+
+
+function confusionMatrix(outputs::Array{Bool,2}, targets::Array{Bool,2}; weighted::Bool=true)
+    @assert(size(outputs)==size(targets));
+    numClasses = size(targets,2);
+    # Nos aseguramos de que no hay dos columnas
+    @assert(numClasses!=2);
+    if (numClasses==1)
+        return confusionMatrix(outputs[:,1], targets[:,1]);
+    else
+        # Nos aseguramos de que en cada fila haya uno y sólo un valor a true
+        @assert(all(sum(outputs, dims=2).==1));
+        # Reservamos memoria para las metricas de cada clase, inicializandolas a 0 porque algunas posiblemente no se calculen
+        recall      = zeros(numClasses);
+        specificity = zeros(numClasses);
+        precision   = zeros(numClasses);
+        NPV         = zeros(numClasses);
+        F1          = zeros(numClasses);
+        # Reservamos memoria para la matriz de confusion
+        confMatrix  = Array{Int64,2}(undef, numClasses, numClasses);
+        # Calculamos el numero de patrones de cada clase
+        numInstancesFromEachClass = vec(sum(targets, dims=1));
+        # Calculamos las metricas para cada clase, esto se haria con un bucle similar a "for numClass in 1:numClasses" que itere por todas las clases
+        #  Sin embargo, solo hacemos este calculo para las clases que tengan algun patron
+        #  Puede ocurrir que alguna clase no tenga patrones como consecuencia de haber dividido de forma aleatoria el conjunto de patrones entrenamiento/test
+        #  En aquellas clases en las que no haya patrones, los valores de las metricas seran 0 (los vectores ya estan asignados), y no se tendran en cuenta a la hora de unir estas metricas
+        for numClass in findall(numInstancesFromEachClass.>0)
+            # Calculamos las metricas de cada problema binario correspondiente a cada clase y las almacenamos en los vectores correspondientes
+            (_, _, recall[numClass], specificity[numClass], precision[numClass], NPV[numClass], F1[numClass], _) = confusionMatrix(outputs[:,numClass], targets[:,numClass]);
+        end;
+
+        # Reservamos memoria para la matriz de confusion
+        confMatrix = Array{Int64,2}(undef, numClasses, numClasses);
+        # Calculamos la matriz de confusión haciendo un bucle doble que itere sobre las clases
+        for numClassTarget in 1:numClasses, numClassOutput in 1:numClasses
+            # Igual que antes, ponemos en las filas los que pertenecen a cada clase (targets) y en las columnas los clasificados (outputs)
+            confMatrix[numClassTarget, numClassOutput] = sum(targets[:,numClassTarget] .& outputs[:,numClassOutput]);
+        end;
+
+        # Aplicamos las forma de combinar las metricas macro o weighted
+        if weighted
+            # Calculamos los valores de ponderacion para hacer el promedio
+            weights = numInstancesFromEachClass./sum(numInstancesFromEachClass);
+            recall      = sum(weights.*recall);
+            specificity = sum(weights.*specificity);
+            precision   = sum(weights.*precision);
+            NPV         = sum(weights.*NPV);
+            F1          = sum(weights.*F1);
+        else
+            # No realizo la media tal cual con la funcion mean, porque puede haber clases sin instancias
+            #  En su lugar, realizo la media solamente de las clases que tengan instancias
+            numClassesWithInstances = sum(numInstancesFromEachClass.>0);
+            recall      = sum(recall)/numClassesWithInstances;
+            specificity = sum(specificity)/numClassesWithInstances;
+            precision   = sum(precision)/numClassesWithInstances;
+            NPV         = sum(NPV)/numClassesWithInstances;
+            F1          = sum(F1)/numClassesWithInstances;
+        end;
+        # Precision y tasa de error las calculamos con las funciones definidas previamente
+        acc = accuracy(outputs, targets; dataInRows=true);
+        errorRate = 1 - acc;
+
+        return (acc, errorRate, recall, specificity, precision, NPV, F1, confMatrix);
+    end;
+end;
+
+
+function confusionMatrix(outputs::Array{Any,1}, targets::Array{Any,1}; weighted::Bool=true)
+    # Comprobamos que todas las clases de salida esten dentro de las clases de las salidas deseadas
+    @assert(all([in(output, unique(targets)) for output in outputs]));
+    classes = unique(targets);
+    # Es importante calcular el vector de clases primero y pasarlo como argumento a las 2 llamadas a oneHotEncoding para que el orden de las clases sea el mismo en ambas matrices
+    return confusionMatrix(oneHotEncoding(outputs, classes), oneHotEncoding(targets, classes); weighted=weighted);
+end;
+
+confusionMatrix(outputs::Array{Float64,2}, targets::Array{Bool,2}; weighted::Bool=true) = confusionMatrix(classifyOutputs(outputs), targets; weighted=weighted);
+
+# De forma similar a la anterior, añado estas funcion porque las RR.NN.AA. dan la salida como matrices de valores Float32 en lugar de Float64
+# Con estas funcion se pueden usar indistintamente matrices de Float32 o Float64
+confusionMatrix(outputs::Array{Float32,2}, targets::Array{Bool,2}; weighted::Bool=true) = confusionMatrix(convert(Array{Float64,2}, outputs), targets; weighted=weighted);
+printConfusionMatrix(outputs::Array{Float32,2}, targets::Array{Bool,2}; weighted::Bool=true) = printConfusionMatrix(convert(Array{Float64,2}, outputs), targets; weighted=weighted);
+
+# Funciones auxiliares para visualizar por pantalla la matriz de confusion y las metricas que se derivan de ella
+function printConfusionMatrix(outputs::Array{Bool,2}, targets::Array{Bool,2}; weighted::Bool=true)
+    (acc, errorRate, recall, specificity, precision, NPV, F1, confMatrix) = confusionMatrix(outputs, targets; weighted=weighted);
+    numClasses = size(confMatrix,1);
+    writeHorizontalLine() = (for i in 1:numClasses+1 print("--------") end; println(""); );
+    writeHorizontalLine();
+    print("\t| ");
+    if (numClasses==2)
+        println(" - \t + \t|");
+    else
+        print.("Cl. ", 1:numClasses, "\t| ");
+    end;
+    println("");
+    writeHorizontalLine();
+    for numClassTarget in 1:numClasses
+        # print.(confMatrix[numClassTarget,:], "\t");
+        if (numClasses==2)
+            print(numClassTarget == 1 ? " - \t| " : " + \t| ");
+        else
+            print("Cl. ", numClassTarget, "\t| ");
+        end;
+        print.(confMatrix[numClassTarget,:], "\t| ");
+        println("");
+        writeHorizontalLine();
+    end;
+    println("Accuracy: ", acc);
+    println("Error rate: ", errorRate);
+    println("Recall: ", recall);
+    println("Specificity: ", specificity);
+    println("Precision: ", precision);
+    println("Negative predictive value: ", NPV);
+    println("F1-score: ", F1);
+    return (acc, errorRate, recall, specificity, precision, NPV, F1, confMatrix);
+end;
+printConfusionMatrix(outputs::Array{Float64,2}, targets::Array{Bool,2}; weighted::Bool=true) =  printConfusionMatrix(classifyOutputs(outputs), targets; weighted=weighted)
+
+function selectBestAnn(topology::Array{Array{Int64,1},1},learningRate::Float64,
+                        numMaxEpochs::Int,validationRatio::Float64,
+                        testRatio::Float64,maxEpochsVal::Int,timesRepeated::Int)
+    for j in 1:length(topology)
+        for i in 0:timesRepeated
+            #newInputs = normalizeMinMax(inputs);
+            normalizeMinMax!(inputs);
+            @assert(all(minimum(inputs, dims=1) .== 0));
+            @assert(all(maximum(inputs, dims=1) .== 1));
+
+            (trainingIndices, validationIndices, testIndices) = holdOut(size(inputs,1), validationRatio, testRatio);
+            if(i==timesRepeated)
+                println(trainingIndices)
+                println(validationIndices)
+                println(testIndices)
+            end
+            trainingInputs    = inputs[trainingIndices,:];
+            validationInputs  = inputs[validationIndices,:];
+            testInputs        = inputs[testIndices,:];
+            trainingTargets   = targets[trainingIndices,:];
+            validationTargets = targets[validationIndices,:];
+            testTargets       = targets[testIndices,:];
+
+            (ann, trainingLosses, validationLosses, testLosses, trainingAccuracies) = trainClassANN(topology[j],
+                trainingInputs,   trainingTargets,
+                validationInputs, validationTargets,
+                testInputs,       testTargets;
+                maxEpochs=numMaxEpochs, learningRate=learningRate, maxEpochsVal=maxEpochsVal, showText=true);
+
+            weights = BSON.load("myweights.bson")
+            params(ann) = weights
+
+            results=plot()
+            plot!(results,1:length(trainingLosses),trainingLosses, xaxis="Epoch",yaxis="Loss",title="Losses iteration " * string(i)*" Top: "*string(topology[j]), label="Training")
+            plot!(results,1:length(validationLosses),validationLosses, label="Validation")
+            plot!(results,1:length(testLosses),testLosses, label="Test")
+            display(results)
+
+            trainingOutputs = collect(ann(trainingInputs')');
+            printConfusionMatrix(trainingOutputs, trainingTargets; weighted=true);
+
+            if i == 0 && j==0
+                global trainingLossesB = trainingLosses;
+                global validationLossesB = validationLosses;
+                global testLossesB = testLosses;
+            end
+
+            if (mean(validationLosses) <= mean(validationLossesB))
+                global weights = params(ann)
+                local finalann=deepcopy(ann)
+                global finalIteration=deepcopy(i);
+                global finalTrainingLosses=deepcopy(trainingLosses);
+                global finalValidationLosses=deepcopy(validationLosses);
+                global finalTestLosses=deepcopy(testLosses);
+                global finalTopology=deepcopy(topology[j])
+                #using BSON: @save
+                BSON.@save "myweights.bson" weights
+
+                trainingLossesB = trainingLosses
+                validationLossesB = validationLosses
+                testLossesB = testLosses
+                println("SAVED WEIGHTS ------------------------------------------------------------------------")
+                println("Iteration ",i)
+                println(mean(trainingLossesB))
+                println(mean(validationLossesB))
+                println(mean(testLossesB))
+            end
+        end
+    end;
+    return finalann,finalIteration,finalTopology,finalTrainingLosses,finalValidationLosses,finalTestLosses
+end;
+
 dataset=loadTrainingDataset();
 inputs=dataset[:,1:6];
 inputs=Float64.(inputs);
@@ -330,60 +562,20 @@ println("Tamaño de la matriz de salidas deseadas despues de codificar: ", size(
 @assert (size(inputs,1)==size(targets,1)) "Las matrices de entradas y salidas deseadas no tienen el mismo numero de filas"
 
 
-topology = [4, 3];
+topology = [[4, 3],[5,6]];
 learningRate = 0.01;
 numMaxEpochs = 1000;
 validationRatio = 0.2;
 testRatio = 0.2;
 maxEpochsVal = 100;
-timesRepeated = 10;
+timesRepeated = 1;
 
-for i in 0:timesRepeated
-    #newInputs = normalizeMinMax(inputs);
-    normalizeMinMax!(inputs);
-    @assert(all(minimum(inputs, dims=1) .== 0));
-    @assert(all(maximum(inputs, dims=1) .== 1));
+local ann;
 
-    (trainingIndices, validationIndices, testIndices) = holdOut(size(inputs,1), validationRatio, testRatio);
-    trainingInputs    = inputs[trainingIndices,:];
-    validationInputs  = inputs[validationIndices,:];
-    testInputs        = inputs[testIndices,:];
-    trainingTargets   = targets[trainingIndices,:];
-    validationTargets = targets[validationIndices,:];
-    testTargets       = targets[testIndices,:];
-
-    (ann, trainingLosses, validationLosses, testLosses, trainingAccuracies) = trainClassANN(topology,
-        trainingInputs,   trainingTargets,
-        validationInputs, validationTargets,
-        testInputs,       testTargets;
-        maxEpochs=numMaxEpochs, learningRate=learningRate, maxEpochsVal=maxEpochsVal, showText=true);
-
-    weights = BSON.load("myweights.bson")
-    params(ann) = weights
-
-    results=plot()
-    plot!(results,1:length(trainingLosses),trainingLosses, xaxis="Epoch",yaxis="Loss",title="Losses iteration " * string(i), label="Training")
-    plot!(results,1:length(validationLosses),validationLosses, label="Validation")
-    plot!(results,1:length(testLosses),testLosses, label="Test")
-    display(results)
-
-    if i == 0
-        global trainingLossesB = trainingLosses;
-        global validationLossesB = validationLosses;
-        global testLossesB = testLosses;
-    end
-
-    if mean(mean(trainingLosses) + mean(validationLosses) + mean(testLosses)) <= mean(mean(trainingLossesB) + mean(validationLossesB) + mean(testLossesB))
-        global weights = params(ann)
-        using BSON: @save
-        @save "myweights.bson" weights
-
-        trainingLossesB = trainingLosses
-        validationLossesB = validationLosses
-        testLossesB = testLosses
-        println("SAVED WEIGHTS ------------------------------------------------------------------------")
-        println(mean(trainingLossesB))
-        println(mean(validationLossesB))
-        println(mean(testLossesB))
-    end
-end
+(ann,iteration,finalTopology,trainingLosses,validationLosses,testLosses) = selectBestAnn(topology,learningRate,numMaxEpochs,validationRatio,testRatio,maxEpochsVal,timesRepeated)
+finalResults=plot()
+plot!(finalResults,1:length(trainingLosses),trainingLosses, xaxis="Epoch",yaxis="Loss",title="Losses Topology: "*string(finalTopology), label="Training")
+plot!(finalResults,1:length(validationLosses),validationLosses, label="Validation")
+plot!(finalResults,1:length(testLosses),testLosses, label="Test")
+display(finalResults)
+println(iteration)
